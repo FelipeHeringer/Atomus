c53a3df2cf92a389cd7596366687abdea06bcc59
Felipe Heringer <heringerfelipe14@gmail.com>
2025-04-06 19:12:26 -0300

    Modified Main class

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,20 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:frontend/features/auth/data/data_sources/auth_remote_data_source.dart';
+import 'package:frontend/features/auth/data/repository/auth_repository_impl.dart';
+import 'package:frontend/features/auth/domain/use_cases/login_use_case.dart';
+import 'package:frontend/features/auth/domain/use_cases/register_use_case.dart';
+import 'package:frontend/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:frontend/features/auth/presentation/screens/login_screen.dart';
+import 'package:frontend/features/auth/presentation/screens/register_screen.dart';
+import 'package:frontend/features/skills/presentation/screens/skills_profile_screen.dart';
 
 void main() {
-  runApp(const MainApp());
+  final authRepository = AuthRepositoryImpl(authRemoteDataSource: AuthRemoteDataSource());
+  runApp(MyApp(authRepository: authRepository,));
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  final AuthRepositoryImpl authRepository;
+  const MyApp({super.key, required this.authRepository});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return MultiBlocProvider(
+     providers: [
+      BlocProvider(create: (_) => AuthBloc(registerUseCase: RegisterUseCase(authRepository: authRepository), loginUseCase: LoginUseCase(authRepository: authRepository)))
+     ],
+     child: MaterialApp(
+      debugShowCheckedModeBanner: false, 
+      title: "Atomus",
+      theme: ThemeData(
+        colorScheme: ColorScheme.light(
+          primary: Color(0xFF011F27),
+          secondary: Color(0xFF3FBFB2),
+          tertiary:Color(0xFFD94F04),
+        )
       ),
-    );
-  }
+      home: const LoginScreen(),
+      routes: {
+        '/login': (_) => LoginScreen(),
+        '/register': (_) => RegisterScreen(),
+        '/skills': (_) => SkillsProfileScreen(),
+      },
+    )
+  );
+  } 
 }
+
