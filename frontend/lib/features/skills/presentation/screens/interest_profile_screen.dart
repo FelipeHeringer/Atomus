aaca4b35174ee64ff7a3042963aca927c0e7db32
Felipe Heringer <heringerfelipe14@gmail.com>
2025-04-23 20:45:26 -0300

    Added domain Interests options

--- frontend/lib/features/skills/presentation/screens/interest_profile_screen.dart
+++ frontend/lib/features/skills/presentation/screens/interest_profile_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/features/skills/domain/entities/interest_option.dart';
+import 'package:frontend/features/skills/domain/entities/interests_option.dart';
 import 'package:frontend/features/skills/presentation/widget/_button.dart';
 import 'package:google_fonts/google_fonts.dart';
 
